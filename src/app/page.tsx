5140bb7ef7c5c9f49b820a8ac3e1c1ab7b1eb0e5
Soocyang <soociayang_99@hotmail.com>
2025-02-20 14:17:28 +0000

    feat: init next auth with goole oauth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { signOut } from '@/auth';
 import Image from 'next/image';
 
 export default function HomePage() {
   return (
     <main className="grid place-items-center h-screen w-screen">
-      <div>
+      <div className="flex flex-col items-center">
         <h1 className="text-2xl font-bold text-center">Hello World!</h1>
 
         <Image
@@ -12,6 +13,20 @@ export default function HomePage() {
           height={160}
           alt="Logo of the app"
         />
+
+        <form
+          action={async () => {
+            'use server';
+            await signOut();
+          }}
+        >
+          <button
+            type="submit"
+            className="border border-primary hover:bg-secondary p-4 font-semibold rounded-md"
+          >
+            Logout
+          </button>
+        </form>
       </div>
     </main>
   );
