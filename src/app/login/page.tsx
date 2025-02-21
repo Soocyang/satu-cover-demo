0804b81f51a49ee3221cde8a893c2b8f26f2d81e
Soocyang <soociayang_99@hotmail.com>
2025-02-21 06:59:06 +0000

    feat: update login ui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,27 +1,18 @@
-import { auth, signIn } from '@/auth';
+import { auth } from '@/auth';
+import { LoginForm } from '@/components/login-form';
 import { redirect } from 'next/navigation';
 
 export default async function LogIn() {
   const session = await auth();
-  const user = session?.user;
+  const isAuthenticated = session?.user;
 
-  if (user) return redirect('/');
+  if (isAuthenticated) return redirect('/');
 
   return (
-    <div className="w-screen h-screen grid place-items-center">
-      <form
-        action={async () => {
-          'use server';
-          await signIn('google');
-        }}
-      >
-        <button
-          type="submit"
-          className="border border-primary hover:bg-secondary p-4 font-semibold rounded-md"
-        >
-          Login with Google
-        </button>
-      </form>
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
+      </div>
     </div>
   );
 }
