cc6e271a801cf717c61b849652d9feb239981755
Soocyang <soociayang_99@hotmail.com>
2025-02-23 06:52:50 +0000

    feat: add mask email handler with usestate

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,4 +1,4 @@
-import UserCard from '@/components/user-card';
+import UsersList from '@/components/users-list';
 import { getAllUsersData } from './actions';
 
 export default async function HomePage() {
@@ -8,19 +8,7 @@ export default async function HomePage() {
     <div className="w-96 md:w-[720px]">
       <h1 className="text-3xl font-bold mb-4">Users</h1>
 
-      <div className="flex flex-col gap-2">
-        {data.map((item) => (
-          <UserCard
-            key={item.id}
-            id={item.id}
-            email={item.email}
-            first_name={item.first_name}
-            last_name={item.last_name}
-            avatar={item.avatar}
-            isEmailMasked={Math.random() > 0.5}
-          />
-        ))}
-      </div>
+      <UsersList users={data} />
     </div>
   );
 }
